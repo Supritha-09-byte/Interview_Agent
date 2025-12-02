6b3d1470e4582b2c685e6d2a167f91fe38b6944b
Supritha H P <suprithahp04@gmail.com>
2025-12-02 17:12:38 +0530

    Force dynamic rendering for all protected pages

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,6 +1,8 @@
 import { getAssessments } from "@/actions/interview";
 import InterviewView from "./interview-view";
 
+export const dynamic = "force-dynamic";
+
 export default async function InterviewPrepPage() {
   const assessments = await getAssessments();
   return <InterviewView assessments={assessments} />;
